ceedf02cbc91d64b2b01bd6f44f506a02bd69d64
Oyedelehabeeb <hoyedeley4@gmail.com>
2024-04-18 09:28:04 +0100

    To-do App

---
+++ main.js
@@ -0,0 +1,44 @@
+import './style.css'
+
+let input = document.querySelector('input')
+let buttn = document.querySelector('#btn')
+
+
+
+buttn.onclick = function () {
+
+  if (input.value.length === 0) {
+    alert('please enter a to-do')
+  } else {
+
+    document.querySelector('#tasks').innerHTML +=
+
+      `<div id="task">
+  <div class= "span-div">
+    <span class="task-span">
+ ${input.value}
+  </span>
+
+  </div>
+
+  
+<div class="div-delete">
+ <button  class="delete">
+  </button>
+</div>
+ 
+  
+  </div>`;
+
+    let current_tasks = document.querySelectorAll(".delete");
+    for (let i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        document.querySelector('#task').remove();
+      }
+    }
+
+    input.value = '';
+  }
+
+}
+
